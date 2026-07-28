26a1913421e7868a418465c3df44e1e04e70a888
Herzog-XI <egmherzog@gmail.com>
2026-07-28 21:19:41 +0200

    Refactor AFK state to primitive values

--- Features/AfkPlayer.cs
+++ Features/AfkPlayer.cs
@@ -1,26 +1,34 @@
 using System;
-using UnityEngine;
 
 namespace AfkManager.Features
 {
     internal sealed class AfkPlayer
     {
-        public AfkPlayer(Vector3 position, Quaternion rotation)
+        public AfkPlayer(float x, float y, float z, float pitch, float yaw)
         {
-            LastPosition = position;
-            LastRotation = rotation;
-            LastActivity = DateTime.UtcNow;
+            Reset(x, y, z, pitch, yaw);
         }
 
-        public Vector3 LastPosition { get; set; }
-        public Quaternion LastRotation { get; set; }
+        public float LastX { get; private set; }
+        public float LastY { get; private set; }
+        public float LastZ { get; private set; }
+        public float LastPitch { get; private set; }
+        public float LastYaw { get; private set; }
         public DateTime LastActivity { get; set; }
         public bool WarningSent { get; set; }
 
-        public void Reset(Vector3 position, Quaternion rotation)
+        public void UpdateSnapshot(float x, float y, float z, float pitch, float yaw)
         {
-            LastPosition = position;
-            LastRotation = rotation;
+            LastX = x;
+            LastY = y;
+            LastZ = z;
+            LastPitch = pitch;
+            LastYaw = yaw;
+        }
+
+        public void Reset(float x, float y, float z, float pitch, float yaw)
+        {
+            UpdateSnapshot(x, y, z, pitch, yaw);
             LastActivity = DateTime.UtcNow;
             WarningSent = false;
         }
